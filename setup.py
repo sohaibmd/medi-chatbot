2b21da39ec8f5fe870bb1b302da0ad8052fa2856
Mdsohaib0131CL221055 <0131CL221055@jnctbhopal.ac.in>
2024-11-10 15:58:21 +0530

    folder structure added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_namespace_packages, setup
+
+setup(
+    name = 'mediacl chatbot project',
+    version = '0.0.1',
+    author = 'sohaib,kali,anjali,harshita',
+    author_email = 'mrsohaib2244@gmail.com',
+    packages = find_namespace_packages(),
+    install_requires = []
+)
